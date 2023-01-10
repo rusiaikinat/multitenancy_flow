12fcae5e3c4a1c03aebfc693fdbc01891c0ab860
rusiaikinat <t.rusiaikina@haulmont.com>
2023-01-10 15:47:44 +0400

    Initial commit

---
+++ src/test/java/com/company/multitenancy_flow/MultitenancyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.multitenancy_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenancyFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
